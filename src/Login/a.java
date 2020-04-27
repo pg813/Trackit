4d599a56f152667d5df7b3b75b2d9fca755bf75d
pg813 <59560495+pg813@users.noreply.github.com>
2020-04-27 19:58:53 +0530

    Create a.java

---
+++ src/Login/a.java
@@ -0,0 +1,13 @@
+package Login;
+
+public class a {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("welcome to the bmc software");
+		
+		
+	}
+
+}
